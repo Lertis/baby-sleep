166afcb8686fbd44662f0980bd3c9415a09f45c0
Yura Husak <yuriy.husak@soft-innovation.ch>
2023-06-05 11:29:52 +0300

    date time selection

--- src/app/modules/insert-date-time/insert-date-time.module.ts
+++ src/app/modules/insert-date-time/insert-date-time.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import {MatButtonModule} from '@angular/material/button';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker'
 
 import { MainDateTimeComponent } from './main-date-time/main-date-time.component'
 
@@ -15,11 +25,27 @@ const COMPONENTS = [MainDateTimeComponent]
 
 const ANGULAR = [
   CommonModule,
-  RouterModule.forChild(routes)
+  RouterModule.forChild(routes),
+  FormsModule,
+  ReactiveFormsModule
+]
+
+const LIBS = [
+  NgxMaterialTimepickerModule
+]
+
+const MATERIAL = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatButtonModule
 ]
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [...ANGULAR]
+  imports: [...ANGULAR, ...LIBS, ...MATERIAL]
 })
 export class InsertDateTimeModule { }
--- src/app/modules/insert-date-time/main-date-time/main-date-time.component.ts
+++ src/app/modules/insert-date-time/main-date-time/main-date-time.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { Subject, takeUntil } from 'rxjs'
 
 @Component({
   selector: 'app-main-date-time',
   templateUrl: './main-date-time.component.html',
   styleUrls: ['./main-date-time.component.scss']
 })
-export class MainDateTimeComponent {
+export class MainDateTimeComponent implements OnDestroy {
+  private readonly destroy$ = new Subject<void>()
 
+  timeForm = new FormGroup({
+    time: new FormControl(''),
+    date: new FormControl({ disabled: true, value: '' }),
+    currentDay: new FormControl(true),
+  })
+
+  get submitDisabled (): boolean {
+    const { date, time } = { ...this.timeForm.getRawValue() as { date: Date, time: string } }
+    return Object.values({ date, time }).filter(v => Boolean(v)).length < 2
+  }
+
+  private readonly getC = (c: 'time' | 'date' | 'currentDay'): FormControl => this.timeForm.get(c) as FormControl
+
+  constructor () {
+    this.initcurrentDaySubscription()
+  }
+
+  private initcurrentDaySubscription (): void {
+    this.getC('currentDay').valueChanges.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(v => {
+      if (v) {
+        this.getC('date').disable()
+        this.getC('date').setValue(new Date())
+      } else {
+        this.getC('date').enable()
+      }
+    })
+  }
+
+  sumbit () {
+    this.timeForm.reset()
+  }
+
+  ngOnDestroy (): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
 }
